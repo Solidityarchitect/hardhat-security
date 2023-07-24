077e8d9152424337c51e1f1c520cc714dabe8e0f
Solidityarchitect <d12stan@hotmail.com>
2023-07-24 23:48:53 +0800

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,15 +17,12 @@ const MAINNET_RPC_URL =
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const SEPOLIA_RPC_URL =
     process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
-const POLYGON_MAINNET_RPC_URL =
-    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 // optional
 const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
 const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
@@ -59,22 +56,15 @@ module.exports = {
             saveDeployments: true,
             chainId: 1,
         },
-        polygon: {
-            url: POLYGON_MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            saveDeployments: true,
-            chainId: 137,
-        },
     },
     etherscan: {
         // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
         apiKey: {
             sepolia: ETHERSCAN_API_KEY,
-            polygon: POLYGONSCAN_API_KEY,
         },
     },
     gasReporter: {
-        enabled: REPORT_GAS,
+        enabled: false,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
